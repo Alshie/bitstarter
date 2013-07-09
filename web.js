01326cb6aa171d7cd6412e658cd8cd3cc5d55c17
Alan Wallace <alan@ginsoaked.com>
2013-07-09 21:53:33 +0000

    refactored web.js

--- web.js
+++ web.js
@@ -4,11 +4,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(fs.readFileSync('index.html', function(err,data) {
-        console.log(data);
-        return data;
-      }));
-  response.send(buffer.toString());
+  var buffer = new Buffer(fs.readFileSync('index.html', 'utf8'));
+  response.send(buffer.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
